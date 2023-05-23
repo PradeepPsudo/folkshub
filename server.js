f939f43a7b92e0f7aed4dab28cdb9bad90961093
pradeep <pradeep_p@intuit.com>
2023-05-23 11:14:15 +0530

    Changing the hosted URL

--- server.js
+++ server.js
@@ -22,25 +22,57 @@ io.socket.on('connection',function(){
 	});
 });
 */
-var app = require('http').createServer(handler),
-    io = require('socket.io').listen(app),
-    fs = require('fs');
 
- //app.listen(8080);
- app.listen(process.env.PORT || 5000);
 
-function handler(req, res) {
-    fs.readFile('./index.html',
-        function (err, data) {
-            if (err) {
-                res.writeHead(500);
-                return res.end('Error loading index.html');
-            }
-            res.writeHead(200);
-            res.end(data);
-            console.log("Gotta you read me and the content is here");
-        });
-}
+// var app = require('http').createServer(handler),
+//     io = require('socket.io').listen(app),
+//     fs = require('fs');
+
+//  //app.listen(8080);
+//  app.listen(process.env.PORT || 5000);
+
+// function handler(req, res) {
+//     fs.readFile('./index.html',
+//         function (err, data) {
+//             if (err) {
+//                 res.writeHead(500);
+//                 return res.end('Error loading index.html');
+//             }
+//             res.writeHead(200);
+//             res.end(data);
+//             console.log("Gotta you read me and the content is here");
+//         });
+// }
+
+// io.sockets.on('connection', function (socket) {
+// 	console.log("i am inside socket");
+//    /* socket.emit('news', {
+//         hello: 'world'
+//     });*/
+//     //socket.on('connection', function (data) {
+//         socket.on("clientMsg",function(data){
+//         	socket.emit("serverMsg",data);
+//         	socket.broadcast.emit("serverMsg", data);
+//         });
+//         socket.on("sender", function (data) {
+// 		    socket.emit("sender", data);
+// 		    socket.broadcast.emit("sender", data); //Broadcast the user typing to 
+// 		});
+//     });
+//});
+
+
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
+});
+
+app.listen(process.env.PORT || 5000);
+
 
 io.sockets.on('connection', function (socket) {
 	console.log("i am inside socket");
@@ -56,5 +88,4 @@ io.sockets.on('connection', function (socket) {
 		    socket.emit("sender", data);
 		    socket.broadcast.emit("sender", data); //Broadcast the user typing to 
 		});
-    });
-//});
+});
